69c31f50f63275449d82c1af555b5cae2f9ddd3b
friedballerina <aleksandra.ikonnikova@clevon.com>
2024-01-08 18:33:20 +0200

    started network service

---
+++ lib/services/network.dart
@@ -0,0 +1,20 @@
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+
+Future<void> fetchData() async {
+  log('fetchData');
+  try {
+    var url = Uri.parse(
+        'http://127.0.0.1:5001/stocks_list'); 
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      print('Response: ${response.body}');
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+}
